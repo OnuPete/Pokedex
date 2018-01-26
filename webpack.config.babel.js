74b8b8af6f5385ab327c9bd32ec57b36b8c126cc
OnuPete <pcr137@case.edu>
2018-01-26 14:21:41 -0800

    Init commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,40 @@
+import webpack from 'webpack'
+import path from 'path'
+
+export default {
+  entry: [
+    'react-hot-loader/patch',
+    './src',
+  ],
+  output: {
+    filename: 'js/bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: `http://localhost:${8000}/dist/`,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+    ],
+  },
+  devtool: isProd ? false : 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    port: 8000,
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+}
